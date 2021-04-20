acd152c287181d85711be31f03283cfbf072fa4f
yaacobliyana <yaacobliyana@gmail.com>
2021-04-20 14:19:20 +0800

    update files

--- sketch/testmcp.py
+++ sketch/testmcp.py
@@ -7,16 +7,16 @@ from adafruit_mcp3xxx.analog_in import AnalogIn
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 cs = digitalio.DigitalInOut(board.D22)
 mcp = MCP.MCP3008(spi, cs)
-channel = AnalogIn(mcp, MCP.P2)
+channel = AnalogIn(mcp, MCP.P0)
 
-v1 = channel.voltage
-bar = (66.7*(v1-0.5))
 
 switch = True
 while (switch == True):
+    v1 = channel.voltage
+    bar = (78*(v1-0.4787))
     print('Raw ADC Value: ', channel.value)
     print('ADC Voltage: ' + str(channel.voltage) + 'V')
     print('Pressure: ' + str(int(bar)) + 'bar')
-    time.sleep(0.5)
+    time.sleep(1)
     if switch == False:
         break
